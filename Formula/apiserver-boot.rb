c7e4bc4efb07fe6964a76366b6003516ad035641
yue9944882 <291271447@qq.com>
2020-07-27 15:11:17 +0800

    overwrite

--- Formula/apiserver-boot.rb
+++ Formula/apiserver-boot.rb
@@ -2,20 +2,17 @@
 class ApiserverBoot < Formula
     desc "Scaffolding Toolkit for APIServer Aggregation"
     homepage "https://github.com/kubernetes-sigs/apiserver-builder-alpha"
-    url "https://github.com/GoogleContainerTools/kpt/archive/v0.31.0.tar.gz"
-    sha256 "1e4884e6a24a917fb62a5eac53a02c92ff82b7625ac4dc7f80d1b1cd4d7b5a9c"
     if OS.mac?
-      url "https://github.com/kubernetes-sigs/apiserver-builder-alpha/archive/v1.18.0.tar.gz"
-      sha256 "c1d2dcae03ab37ec71814f70476aa821f64a119f31072047050abf863fd32ae0"
+      url "https://github.com/kubernetes-sigs/apiserver-builder-alpha.git"
+        :using => :git,
+        :revision => "95dca1d34e91d6e76c50fa4f272a77f573fd7558"
+      depends_on "bazel" => :build
+      def install
+        system "bazel","build","--platforms=@io_bazel_rules_go//go/toolchain:darwin_amd64","cmd:apiserver-builder"
+      end
     elsif OS.linux?
       #url "https://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/linux64.tbl2asn.gz"
       #sha256 "38560dd0764d1cfa7e139c65285b3194bacaa4bd8ac09f60f5e2bb8027cc6ca2"
-    end
-  
-    depends_on "bazel" => :build
-  
-    def install
-      system "make install"
     end
   
     test do
